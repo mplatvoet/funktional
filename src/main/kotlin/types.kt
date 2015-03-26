457d0a19c7510d6677a89150df233b93ea9d2272
mplatvoet <mplatvoet@gmail.com>
2015-03-26 09:18:43 +0100

    explicitly require the use of non-nullable type

--- src/main/kotlin/types.kt
+++ src/main/kotlin/types.kt
@@ -1,68 +1,68 @@
 package nl.mplatvoet.funktional.types
 
 public trait Functor<A : Any> {
-    fun map<B>(fn: (A) -> B): Functor<B>
+    fun map<B : Any>(fn: (A) -> B): Functor<B>
 }
 
 public trait Monad<A : Any> {
-    fun bind<B>(fn: (A) -> Functor<B>): Functor<B>
+    fun bind<B : Any>(fn: (A) -> Functor<B>): Functor<B>
 }
 
 public trait Applicative<A : Any> {
-    fun apply<B>(f: Functor<(A) -> B>): Functor<B>
+    fun apply<B : Any>(f: Functor<(A) -> B>): Functor<B>
 }
 
 
 public open class Just<A>(val value: A) : Functor<A> {
-    override fun <B> map(fn: (A) -> B): Functor<B> = Just(fn(value))
+    override fun <B : Any> map(fn: (A) -> B): Functor<B> = Just(fn(value))
 }
 
-public trait Option<A> : Functor<A> {
+public trait Option<A : Any> : Functor<A> {
     companion object {
         fun of<A : Any>(value: A?): Option<A> = if (value == null) None() else Some(value)
     }
 
-    override fun map<B>(fn: (A) -> B): Option<B>
+    override fun map<B : Any>(fn: (A) -> B): Option<B>
 }
 
 public class None<A> : Option<A> {
-    override fun <B> map(fn: (A) -> B): Option<B> = None()
+    override fun <B : Any> map(fn: (A) -> B): Option<B> = None()
 }
 
 public class Some<A>(value: A) : Just<A>(value), Option<A> {
-    override fun <B> map(fn: (A) -> B): Option<B> = Some(fn(value))
+    override fun <B : Any> map(fn: (A) -> B): Option<B> = Some(fn(value))
 }
 
 
-public trait Either<A, B> : Functor<B> {
+public trait Either<A : Any, B : Any> : Functor<B> {
     companion object {
-        fun <A, B> left(value: A) = Left<A, B>(value)
-        fun <A, B> right(value: B) = Right<A, B>(value)
+        fun <A : Any, B : Any> left(value: A) = Left<A, B>(value)
+        fun <A : Any, B : Any> right(value: B) = Right<A, B>(value)
     }
 
-    override fun map<C>(fn: (B) -> C): Either<A, C>
+    override fun map<C : Any>(fn: (B) -> C): Either<A, C>
 }
 
-public open class Left<A, B>(val value: A) : Either<A, B> {
-    override fun map<C>(fn: (B) -> C): Either<A, C> = Left<A, C>(value)
+public open class Left<A : Any, B : Any>(val value: A) : Either<A, B> {
+    override fun map<C : Any>(fn: (B) -> C): Either<A, C> = Left<A, C>(value)
 }
 
-public open class Right<A, B>(val value: B) : Either<A, B> {
-    override fun map<C>(fn: (B) -> C): Either<A, C> = Right<A, C>(fn(value))
+public open class Right<A : Any, B : Any>(val value: B) : Either<A, B> {
+    override fun map<C : Any>(fn: (B) -> C): Either<A, C> = Right<A, C>(fn(value))
 }
 
-public trait Try<A> : Either<Throwable, A> {
-    override fun map<B>(fn: (A) -> B): Try<B>
+public trait Try<A : Any> : Either<Throwable, A> {
+    override fun map<B : Any>(fn: (A) -> B): Try<B>
 }
 
-public class Success<A>(value: A) : Right<Throwable, A> (value), Try<A> {
-    override fun <B> map(fn: (A) -> B): Try<B> = try {
+public class Success<A : Any>(value: A) : Right<Throwable, A> (value), Try<A> {
+    override fun <B : Any> map(fn: (A) -> B): Try<B> = try {
         Success(fn(value))
     } catch (t: Throwable) {
         Failure(t)
     }
 }
 
-public class Failure<A>(value: Throwable) : Left<Throwable, A> (value), Try<A> {
-    override fun <B> map(fn: (A) -> B): Try<B> = Failure(value)
+public class Failure<A : Any>(value: Throwable) : Left<Throwable, A> (value), Try<A> {
+    override fun <B : Any> map(fn: (A) -> B): Try<B> = Failure(value)
 }
