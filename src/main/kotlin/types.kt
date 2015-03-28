f66c6b264dccdc55717f3fc787d85e93a31a96f4
mplatvoet <mplatvoet@gmail.com>
2015-03-28 10:51:06 +0100

    Take on monads in Kotlin, annoyingly limited by the capabilities of the Jvm.

--- src/main/kotlin/types.kt
+++ src/main/kotlin/types.kt
@@ -13,24 +13,28 @@ public trait Applicative<A : Any> {
 }
 
 
-public open class Just<A>(val value: A) : Functor<A> {
+public open class Just<A>(val value: A) : Functor<A>,  Monad<A> {
     override fun <B : Any> map(fn: (A) -> B): Just<B> = Just(fn(value))
+    override fun <B : Any> bind(fn: (A) -> Monad<B>): Just<B> = bindMonad(value, fn)
 }
 
-public trait Option<A : Any> : Functor<A> {
+public trait Option<A : Any> : Functor<A>, Monad<A> {
     companion object {
         fun of<A : Any>(value: A?): Option<A> = if (value == null) None() else Some(value)
     }
 
     override fun map<B : Any>(fn: (A) -> B): Option<B>
+    override fun bind<B : Any>(fn: (A) -> Monad<B>): Option<B>
 }
 
 public class None<A> : Option<A> {
     override fun <B : Any> map(fn: (A) -> B): Option<B> = None()
+    override fun bind<B : Any>(fn: (A) -> Monad<B>): None<B> = None()
 }
 
-public class Some<A>(value: A) : Just<A>(value), Option<A> {
+public open class Some<A>(value: A) : Just<A>(value), Option<A> {
     override fun <B : Any> map(fn: (A) -> B): Some<B> = Some(fn(value))
+    override fun bind<B : Any>(fn: (A) -> Monad<B>): Some<B> = bindMonad(value, fn)
 }
 
 
@@ -47,7 +51,7 @@ public open class Left<A : Any, B : Any>(val value: A) : Either<A, B> {
     override fun map<C : Any>(fn: (B) -> C): Left<A, C> = Left<A, C>(value)
 }
 
-public open class Right<A : Any, B : Any>(val value: B) : Either<A, B> {
+public open class Right<A : Any, B : Any>(value: B) : Either<A, B>, Some<B>(value) {
     override fun map<C : Any>(fn: (B) -> C): Right<A, C> = Right<A, C>(fn(value))
 }
 
@@ -61,4 +65,12 @@ public class Success<A : Any>(value: A) : Right<Throwable, A> (value), Try<A> {
 
 public class Failure<A : Any>(value: Throwable) : Left<Throwable, A> (value), Try<A> {
     override fun <B : Any> map(fn: (A) -> B): Failure<B> = Failure(value)
+}
+
+private inline fun <A:Any, B:Any, reified T:Monad<B>> bindMonad(value: A, fn: (A) -> Monad<B>): T {
+    val monad = fn(value)
+    return when (monad) {
+        is T -> monad
+        else -> throw IllegalStateException("Resulting Monad is not of type ${javaClass<T>()} but ${monad.javaClass}")
+    }
 }
